40c248108f09ed05a592fa3907e203bb03159cc7
Enzo Gaban <enzogaban@gmail.com>
2024-03-03 11:28:28 -0300

    adds copilot

---
+++ plugins/cmp.lua
@@ -0,0 +1,21 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    {
+      "zbirenbaum/copilot-cmp",
+      config = function()
+        require("copilot_cmp").setup()
+      end,
+    },
+  },
+  opts = {
+    sources = {
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+      { name = "buffer" },
+      { name = "nvim_lua" },
+      { name = "path" },
+      { name = "copilot" },
+    },
+  },
+}
